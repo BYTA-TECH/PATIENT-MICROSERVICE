36de2266fd923e3640d03fedb9a1a618879d1167
rafeekbytatech <abdul.rafeek@lxisoft.com>
2019-10-31 21:20:13 +0530

    refactored dmsCore

--- src/main/java/com/bytatech/ayoos/client/dms_core/api/SitesApiClient.java
+++ src/main/java/com/bytatech/ayoos/client/dms_core/api/SitesApiClient.java
@@ -1,7 +1,7 @@
 package com.bytatech.ayoos.client.dms_core.api;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import com.bytatech.ayoos.client.dmscore.ClientConfiguration;
+import com.bytatech.ayoos.client.dms_core.ClientConfiguration;
 
 @FeignClient(name="${dmsCore.name:dmsCore}", url="${dmsCore.url:http://35.238.35.19:8013/alfresco/api/-default-/public/alfresco/versions/1}", configuration = ClientConfiguration.class)
 public interface SitesApiClient extends SitesApi {
