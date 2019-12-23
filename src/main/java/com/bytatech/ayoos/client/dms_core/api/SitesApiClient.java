1ff0cb15379d86b0bc05fc8d28f8ff56943f7aba
rafeekbytatech <abdul.rafeek@lxisoft.com>
2019-12-23 11:49:40 +0530

    add new alfresco

--- src/main/java/com/bytatech/ayoos/client/dms_core/api/SitesApiClient.java
+++ src/main/java/com/bytatech/ayoos/client/dms_core/api/SitesApiClient.java
@@ -3,6 +3,6 @@ package com.bytatech.ayoos.client.dms_core.api;
 import org.springframework.cloud.openfeign.FeignClient;
 import com.bytatech.ayoos.client.dms_core.ClientConfiguration;
 
-@FeignClient(name="${dmsCore.name:dmsCore}", url="${dmsCore.url:http://35.238.35.19:8013/alfresco/api/-default-/public/alfresco/versions/1}", configuration = ClientConfiguration.class)
+@FeignClient(name="${dmsCore.name:dmsCore}", url="${dmsCore.url:https://aozd73.trial.alfresco.com/alfresco/api/-default-/public/alfresco/versions/1}", configuration = ClientConfiguration.class)
 public interface SitesApiClient extends SitesApi {
 }
